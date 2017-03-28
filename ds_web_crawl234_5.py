170e19fbf65fd70eed445c2938d30825ca506010
bagell3 <bagell4@naver.com>
2017-03-29 03:48:15 +0900

    Create ds_web_crawl234_5.py

---
+++ ds_web_crawl234_5.py
@@ -0,0 +1,23 @@
+
+import lxml.html
+from lxml.cssselect import CSSSelector
+import requests
+link=requests.get('http://www.ieee.org/conferences_events/conferences/search/index.html')
+
+html = lxml.html.fromstring(link.text)
+study = CSSSelector('div.content-r-full table.nogrid-nopad tr p>a[href]')
+lines = study(html)
+n=0
+for line in lines:
+    if n%3 == 0:
+        print "Conference name: ",line.text
+        print "============="
+        n+=1
+    elif n%3 ==1:
+        print "Conference Date: ",line.text
+        print "============="
+        n+=1
+    elif n%3 ==2:
+        print "Location: ",line.text
+        print "============="
+        n+=1
